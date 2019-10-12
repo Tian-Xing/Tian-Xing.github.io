9559f8441b7e5aea7861ff7c1c0e40a980f106c4
Tian-Xing <chen_sheng_qian@outlook.com>
2019-10-12 20:20:56 +0800

    Site updated: 2019-10-12 20:20:55

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -441,16 +441,12 @@ function checkBgImgCookie () {
       $('#night-mode-cover').css('visibility', 'hidden')
       $('body').css('background-image', 'url(' + bgurl + ')')
       $('.blank').css('background-color', 'rgba(255,255,255,1)')
-      $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-      $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
     } else if (bgurl == 'http://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302') {
       mashiro_global.variables.skinSecter = true
       mashiro_global.variables.isNight = true
       $('#night-mode-cover').css('visibility', 'hidden')
       $('body').css('background-image', 'url(' + bgurl + ')')
       $('.blank').css('background-color', 'rgba(255,255,255,1)')
-      $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-      $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
     } else {}
   } else {
     return false
@@ -531,8 +527,6 @@ $(document).ready(function () {
       $('#night-mode-cover').css('visibility', 'hidden')
       $('body').css('background-image', 'url(' + url + ')')
       $('.blank').css('background-color', 'rgba(255,255,255,1)')
-      $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-      $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
       $('#banner_wave_1').addClass('banner_wave_hide_fit_skin')
       $('#banner_wave_2').addClass('banner_wave_hide_fit_skin')
       closeSkinMenu()
@@ -547,8 +541,6 @@ $(document).ready(function () {
       $('#night-mode-cover').css('visibility', 'hidden')
       $('body').css('background-image', 'url(' + url + ')')
       $('.blank').css('background-color', 'rgba(255,255,255,1)')
-      $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-      $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
       $('#banner_wave_1').addClass('banner_wave_hide_fit_skin')
       $('#banner_wave_2').addClass('banner_wave_hide_fit_skin')
       closeSkinMenu()
@@ -567,8 +559,6 @@ $(document).ready(function () {
     $('#night-mode-cover').css('visibility', 'hidden')
     $('body').css('background-image', 'none')
     $('.blank').css('background-color', 'rgba(255,255,255,.0)')
-    $('.pattern-center-sakura').removeClass('pattern-center-sakura').addClass('pattern-center')
-    $('.headertop-bar-sakura').removeClass('headertop-bar-sakura').addClass('headertop-bar')
     $('#banner_wave_1').removeClass('banner_wave_hide_fit_skin')
     $('#banner_wave_2').removeClass('banner_wave_hide_fit_skin')
     closeSkinMenu()
@@ -580,8 +570,6 @@ $(document).ready(function () {
     $('body').css('background-image', 'url(https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/starry_sky.png)')
     $('.blank').css('background-color', 'rgba(255,255,255,.8)')
     $('#night-mode-cover').css('visibility', 'visible')
-    $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-    $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
     $('#banner_wave_1').addClass('banner_wave_hide_fit_skin')
     $('#banner_wave_2').addClass('banner_wave_hide_fit_skin')
     closeSkinMenu()
@@ -900,8 +888,6 @@ var pjaxInit = function () {
     reload_show_date_time()
   } catch (e) {}
   if (mashiro_global.variables.skinSecter === true) {
-    $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
-    $('.headertop-bar').removeClass('headertop-bar').addClass('headertop-bar-sakura')
     if (mashiro_global.variables.isNight) {
       $('.blank').css('background-color', 'rgba(255,255,255,1)')
       $('.toc').css('background-color', 'rgba(255,255,255,0.8)')
